c76649af4cb1ac571f2a5d03250411066c43cdba
glebvvss <33166555+Glebvvss@users.noreply.github.com>
2018-11-08 21:25:38 +0200

    Update IRouter.php

--- vendor/Router/Interfaces/IRouter.php
+++ vendor/Router/Interfaces/IRouter.php
@@ -4,6 +4,12 @@ namespace Service\Router\Interfaces;
 
 interface IRouter {
 
+	public function run();
+	
+	public function setRootDirectory( $nonDefaultRootDirectory );
+	
 	public static function regRoute( string $routeTpl, string $controller );
+	
+	public function addSourceRoutes( $pathToRegistrationRoutesFile );
 
 }
